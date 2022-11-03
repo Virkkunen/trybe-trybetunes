a02ff2dc2bf6e2a1abdc7c47d5efa8d4a3b4312c
Virkkunen <vrkknn@vrkknn.net>
2022-11-03 16:00:20 -0300

    req 02 in progress

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
+import { createUser } from '../services/userAPI';
 
 export default class Login extends Component {
+  state = {
+    inputDisabled: true,
+  };
+
+  inputValidation = (e) => {
+    const minLength = 3; // 🙄 no magic number
+    const inputValid = e.target.value.length >= minLength;
+    this.setState({ inputDisabled: !inputValid });
+  };
+
+  sendUser = () => {
+    const input = document.getElementById('login-name').value;
+    return createUser({ name: input });
+  };
+
   render() {
+    const { inputDisabled } = this.state;
+
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        Login
+        <form>
+          <label htmlFor="login-name">
+            <span>Nome: </span>
+            <input
+              type="text"
+              data-testid="login-name-input"
+              id="login-name"
+              onChange={ this.inputValidation }
+            />
+          </label>
+          <button
+            type="button"
+            disabled={ inputDisabled }
+            data-testid="login-submit-button"
+            onClick={ this.sendUser }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     );
   }
 }
